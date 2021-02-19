9d712cbb5c4346297e9bf1b70cfc8b8197e915bc
takasehideki <takase@i.kyoto-u.ac.jp>
2021-02-19 22:08:41 +0900

    add :inets as extra_applications to execute weather/0

--- mix.exs
+++ mix.exs
@@ -23,7 +23,7 @@ defmodule NervesjpRicc.MixProject do
   def application do
     [
       mod: {NervesjpRicc.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :inets]
     ]
   end
 
