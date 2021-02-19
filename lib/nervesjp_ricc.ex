1101f192e004d28fee7224256e182299d21cf56b
takasehideki <takase@i.kyoto-u.ac.jp>
2021-02-19 18:16:32 +0900

    mix nerves.new nervesjp_ricc --target rpi0 --target rpi3 --target rpi4

---
+++ lib/nervesjp_ricc.ex
@@ -0,0 +1,18 @@
+defmodule NervesjpRicc do
+  @moduledoc """
+  Documentation for NervesjpRicc.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NervesjpRicc.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/nervesjp_ricc_test.exs
@@ -0,0 +1,8 @@
+defmodule NervesjpRiccTest do
+  use ExUnit.Case
+  doctest NervesjpRicc
+
+  test "greets the world" do
+    assert NervesjpRicc.hello() == :world
+  end
+end
